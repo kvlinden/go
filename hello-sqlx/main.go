e98c17d6eb37ab8d2918ced92c54cc3bc19d7627
kvlinden <kvlinden@calvin.edu>
2017-07-26 19:50:26 -0400

    working on gocms where-condition builder...

--- hello-sqlx/main.go
+++ hello-sqlx/main.go
@@ -4,21 +4,17 @@ Run this API integration test using the test utility option.
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"reflect"
+	"strconv"
 	"time"
 )
 
 // Controller utilities
 // Get/verify URL parameter values and build parameters struct.
 
-type UrlSearchParameters struct {
-	SearchString string
-	SearchInt    int64
-	SearchFloat  float64
-}
 type UrlParameters struct {
-	SearchString       *UrlSearchParameters
+	SearchString       *string
 	IntFilter          *int64
 	FloatFilter        *float64
 	StringFilter       *string
@@ -54,18 +50,43 @@ func createDate(dateString string) *time.Time {
 // Service utilities
 // Convert URL parameter structure to where condition map, ignoring nil values.
 
-type WhereConditions map[string]interface{}
+type SearchConditions map[string]interface{}
+type FilterConditions map[string]interface{}
+
+// Is there a way to handle these using *interface{}? I couldn't get it to work.
+func AddWhereIntCondition(fieldName string, value *int64, filterConditions FilterConditions) {
+	if value != nil {
+		filterConditions[fieldName] = *value
+	}
+}
+func AddWhereFloatCondition(fieldName string, value *float64, filterConditions FilterConditions) {
+	if value != nil {
+		filterConditions[fieldName] = *value
+	}
+}
+func AddWhereStringCondition(fieldName string, value *string, filterConditions FilterConditions) {
+	if value != nil {
+		filterConditions[fieldName] = *value
+	}
+}
+func AddWhereBoolCondition(fieldName string, value *bool, filterConditions FilterConditions) {
+	if value != nil {
+		filterConditions[fieldName] = *value
+	}
+}
+func AddWhereDateCondition(fieldName string, value *time.Time, filterConditions FilterConditions) {
+	if value != nil {
+		filterConditions[fieldName] = *value
+	}
+}
 
 // repository utilities
 
 func main() {
 	// Controller code
 	var urlParameters = UrlParameters{
-		SearchString: &UrlSearchParameters{
-			SearchString: "str1",
-			SearchInt:    1,
-			SearchFloat:  1.0,
-		},
+		SearchString: createString("str1"),
+		//SearchString:       nil,  // The search string is either a (single) string or nil.
 		IntFilter:          createInt64(1),
 		FloatFilter:        createFloat64(1.0),
 		StringFilter:       createString("str2"),
@@ -77,22 +98,66 @@ func main() {
 		BoolIgnoreFilter:   nil,
 		DateIgnoreFilter:   nil,
 	}
-	fmt.Println(fmt.Sprintf("Controller: creates %+v", urlParameters))
+	fmt.Println(fmt.Sprintf("\nController:\n%+v", urlParameters))
 
 	// Services code
-	whereConditions := make(WhereConditions)
-	v := reflect.ValueOf(urlParameters)
-	for i := 0; i < v.NumField(); i++ {
-		fmt.Println(v.Type().Field(i).Name, v.Field(i).Type().String(), v.Field(i).Interface())
-		fmt.Printf("(%v, %T)\n", v.Field(i).Interface(), v.Field(i).Interface())
-		if v.Field(i).Interface() != nil {
-			switch ptr := v.Field(i).Interface().(type) {
-			case *string:
-				fmt.Println(*ptr)
-			}
+	filterConditions := make(FilterConditions)
+	searchConditions := make(SearchConditions)
+	if urlParameters.SearchString != nil {
+		searchConditions["SearchString"] = *urlParameters.SearchString
+		intValue, err := strconv.ParseInt(*urlParameters.SearchString, 10, 64)
+		if err == nil {
+			searchConditions["SearchInt"] = intValue
+		}
+		floatValue, err := strconv.ParseFloat(*urlParameters.SearchString, 64)
+		if err == nil {
+			searchConditions["SearchFloat"] = floatValue
 		}
 	}
-	fmt.Println(whereConditions)
+	AddWhereIntCondition("IntFilter", urlParameters.IntFilter, filterConditions)
+	AddWhereFloatCondition("FloatFilter", urlParameters.FloatFilter, filterConditions)
+	AddWhereStringCondition("StringFilter", urlParameters.StringFilter, filterConditions)
+	AddWhereBoolCondition("BoolFilter", urlParameters.BoolFilter, filterConditions)
+	AddWhereDateCondition("DateFilter", urlParameters.DateFilter, filterConditions)
+	AddWhereIntCondition("IntIgnoreFilter", urlParameters.IntIgnoreFilter, filterConditions)
+	AddWhereFloatCondition("FloatIgnoreFilter", urlParameters.FloatIgnoreFilter, filterConditions)
+	AddWhereStringCondition("StringIgnoreFilter", urlParameters.StringIgnoreFilter, filterConditions)
+	AddWhereBoolCondition("BoolIgnoreFilter", urlParameters.BoolIgnoreFilter, filterConditions)
+	AddWhereDateCondition("DateIgnoreFilter", urlParameters.DateIgnoreFilter, filterConditions)
+	// Generalizing this is probably possible using Reflection, but complicated. This code doesn't work properly.
+	//v := reflect.ValueOf(urlParameters)
+	//for i := 0; i < v.NumField(); i++ {
+	//	fmt.Println(v.Type().Field(i).Name, v.Field(i).Type().String(), v.Field(i).Interface())
+	//	fmt.Printf("(%v, %T)\n", v.Field(i).Interface(), v.Field(i).Interface())
+	//	if v.Field(i).Interface() != nil {
+	//		switch ptr := v.Field(i).Interface().(type) {
+	//		case *string:
+	//			fmt.Println(*ptr)
+	//		}
+	//	}
+	//}
+	fmt.Println(fmt.Sprintf("\nServices:\n%+v \n%+v", searchConditions, filterConditions))
 
 	// Repository code
+	var whereConditionBuffer bytes.Buffer
+	//for key, value := range searchConditions {
+	//	whereConditionBuffer.WriteString(fmt.Sprintf("OR %s = %s ", key, valueString))
+	//}
+	for key, value := range filterConditions {
+		var valueString string
+		switch v := value.(type) {
+		case string:
+			valueString = fmt.Sprintf("'%s'", v)
+		case int64:
+			valueString = fmt.Sprintf("%d", v)
+		case float64:
+			valueString = fmt.Sprintf("%f", v)
+		case bool:
+			valueString = strconv.FormatBool(v)
+		case time.Time:
+			valueString = fmt.Sprintf("'%s'", v.String())
+		}
+		whereConditionBuffer.WriteString(fmt.Sprintf("AND %s = %s ", key, valueString))
+	}
+	fmt.Println(fmt.Sprintf("\nRepositories:\n%s", whereConditionBuffer.String()))
 }
